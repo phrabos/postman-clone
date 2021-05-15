cf5defd9432288a0b46ec52809efefe83ec17283
patrick hrabos <phrabos@gmail.com>
2021-05-15 10:57:17 -0400

    css

--- src/components/postman/History.jsx
+++ src/components/postman/History.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
+// import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from '@material-ui/core/styles';
 import styles from './postman.css';
 
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
 function History({ history, onLiClick, onDeleteClick }) {
+  const classes = useStyles();
   return (
     <div className={styles.history}>
-      <h3>History</h3>
-      <ul>
+      <h3 style={{ margin: '0' }}>History</h3>
+      <ul style={{ margin: '0' }}>
         {!!history.length && history.map((item, i) => {
           return (
             <div key={`${item.url} + ${i}`} className={styles.historyWrapper}>
@@ -19,15 +31,16 @@ function History({ history, onLiClick, onDeleteClick }) {
                 <span className={styles.methodName}>{item.method}</span>
                 <span className={styles.methodClick}>{item.url}</span>
               </li>
-              <span>
-                <IconButton 
-                  aria-label="delete"
-                  onClick={onDeleteClick}
-                  value={i}
-                >
-                  <DeleteIcon className={styles.deleteButton}/>
-                </IconButton>
-              </span>
+
+              <IconButton 
+                aria-label="delete"
+                onClick={onDeleteClick}
+                value={i}
+                className={classes.margin}
+              >
+                <DeleteIcon />
+              </IconButton>
+
             </div>
           );
         })}
